7bcae2f5448168c59f6b5b6fff13532cae982dd4
hong0572-arch <iwingzpro@gmail.com>
2025-12-10 14:13:25 +0900

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'), // 잡지 스타일의 핵심!
+    ],
+};
